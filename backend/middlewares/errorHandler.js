5028e1277291451ea7269734c168e3b01a260dc8
Brandon Pérez <brandonperez@MacBook-Pro-de-Brandon.local>
2026-04-28 19:25:22 -0600

    added error handler middleware

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+import { handleError } from "../controllers/errorController.js"
+
+function errorHandler(err, req, res, next) {
+  const error = handleError(err)
+
+  return res.status(error.statusCode).json({
+    status: 'error',
+    error: error.name,
+    message: error.message
+  })
+}
+
+export default errorHandler
