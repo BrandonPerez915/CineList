52044afcd6e8f48d22f67921cc7ab3cc7b966c06
Brandon Pérez <brandonperez@MacBook-Pro-de-Brandon.local>
2026-04-28 19:28:49 -0600

    modified server file to connect to the db

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
-// Estructura inicial
-console.log("Hola mundo")
+import express from 'express'
+
+import { connectDB } from './config/db.js'
+import apiRouter from './routes/apiRouter.js'
+
+const app = express()
+connectDB()
+
+app.use(express.json())
+app.use('/api', apiRouter)
+
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"))
